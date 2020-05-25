a85adb60b45316f4de20231c799c1a32b8c92648
tamas-polgar <tamas.polgar.dev@gmail.com>
2020-05-25 18:54:12 +0200

    Implement info popup dialog

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InfoPopup from './InfoPopup';
 
 function Footer() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className='share' id='share'>
-      <a href='# ' className='share__i share__i--facebook'>
-        <i className='icon-facebook'></i>
-      </a>
-      <a href='# ' className='share__i share__i--twitter'>
-        <i className='icon-twitter'></i>
-      </a>
-      <div className='share__i share__i--code'>
-        <i className='icon-code'></i>
+    <>
+      <div className='share' id='share'>
+        <a href='# ' className='share__i share__i--facebook'>
+          <i className='icon-facebook'></i>
+        </a>
+        <a href='# ' className='share__i share__i--twitter'>
+          <i className='icon-twitter'></i>
+        </a>
+        <div className='share__i share__i--code'>
+          <i className='icon-code'></i>
+        </div>
+        <div className='share__i share__i--info' onClick={() => setOpen(true)}>
+          <i className='icon-info'></i>
+        </div>
       </div>
-      <div className='share__i share__i--info'>
-        <i className='icon-info'></i>
-      </div>
-    </div>
+
+      <InfoPopup open={open} closeDialog={() => setOpen(false)} />
+    </>
   );
 }
 
--- src/components/InfoPopup.js
+++ src/components/InfoPopup.js
@@ -1,86 +1,54 @@
 import React from 'react';
 
-function InfoPopup() {
+function InfoPopup({ open, closeDialog }) {
   return (
-    <>
-      <div className='screen screen--main'>
-        <div className='language'>
-          <a href='# ' className='language__i language__i--selected'>
-            EN
-          </a>
-          <a href='# ' className='language__i'>
-            FI
-          </a>
-        </div>
-
-        <article className='intro'>
-          <img
-            src='assets/img/logo-en.svg'
-            alt='Esports Champions'
-            className='intor__logo'
-          />
-
-          <div className='intro__desc'>
-            <p>
-              With the League of Legends final drawing more viewers than the
-              last game of the 2016 NBA Finals and total eSports prize pools
-              exceeding $93 million; gaming has transformed from a pastime to a
-              profession.
-            </p>
-            <p>
-              For the elite, competitive gaming has become a profitable career,
-              but what makes these players stand out in a league of their own?
-              Discover just how much these gamers earn, where they’re from and
-              more in The Champions of eSports.
-            </p>
-          </div>
-        </article>
-      </div>
-
-      <div className='popup-wrap'>
-        <div className='popup popup--info'>
-          <button className='popup__close-btn'>
-            <i className='icon-close'></i>
-          </button>
-          <h3 className='popup__title'>Information</h3>
-          <div className='popup__content'>
-            <p>Champions of eSports is brought to you by Unibet. </p>
-            <p>
-              Viewership data has been sourced from{' '}
-              <a
-                target='_blank'
-                href='https://www.statista.com/statistics/490480/global-esports-audience-size-viewer-type/'
-                rel='noopener noreferrer'
-              >
-                Statista
-              </a>
-              .
-            </p>
-            <p>
-              eSport player, game and country data has been sourced from{' '}
-              <a
-                target='_blank'
-                href='http://www.esportsearnings.com/'
-                rel='noopener noreferrer'
-              >
-                eSportsearnings
-              </a>
-              ,{' '}
-              <a
-                target='_blank'
-                href='http://wiki.teamliquid.net/'
-                rel='noopener noreferrer'
-              >
-                TeamLiquid
-              </a>
-              , Newzoo and Twitch.
-            </p>
-            <p>Population data is supplied by WorldBank.org.</p>
-            <p>Information is up-to-date as of February 2017.</p>
-          </div>
+    <div
+      className='popup-wrap'
+      style={{ display: open ? 'block' : 'none' }}
+      onClick={() => closeDialog()}
+    >
+      <div className='popup popup--info'>
+        <button className='popup__close-btn' onClick={() => closeDialog()}>
+          <i className='icon-close'></i>
+        </button>
+        <h3 className='popup__title'>Information</h3>
+        <div className='popup__content'>
+          <p>Champions of eSports is brought to you by Unibet. </p>
+          <p>
+            Viewership data has been sourced from{' '}
+            <a
+              target='_blank'
+              href='https://www.statista.com/statistics/490480/global-esports-audience-size-viewer-type/'
+              rel='noopener noreferrer'
+            >
+              Statista
+            </a>
+            .
+          </p>
+          <p>
+            eSport player, game and country data has been sourced from{' '}
+            <a
+              target='_blank'
+              href='http://www.esportsearnings.com/'
+              rel='noopener noreferrer'
+            >
+              eSportsearnings
+            </a>
+            ,{' '}
+            <a
+              target='_blank'
+              href='http://wiki.teamliquid.net/'
+              rel='noopener noreferrer'
+            >
+              TeamLiquid
+            </a>
+            , Newzoo and Twitch.
+          </p>
+          <p>Population data is supplied by WorldBank.org.</p>
+          <p>Information is up-to-date as of February 2017.</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
