0b8ba4a40a2bb235448ea922fd8f14dcabe374d8
tamas-polgar <tamas.polgar.dev@gmail.com>
2020-05-28 18:54:13 +0200

    Data import for player cards

--- src/components/LeaderboardsTopEarningMen.js
+++ src/components/LeaderboardsTopEarningMen.js
@@ -1,87 +1,116 @@
-import React from 'react';
-import avatarUrl from '../data/images/1-universe.png';
-import teamLogo from '../data/teams/Evil-Geniuses.jpg';
+import React, { useContext } from 'react';
+
+import VisualizationContext from '../contexts/VisualizationContext';
+import TranslationContext from '../contexts/TranslationContext';
 
 function LeaderboardsTopEarningMen() {
-  function generateCards(count) {
-    return [...Array(count)].map((key, index) => (
-      <div key={index} className='p-card'>
-        <div className='p-card__inner'>
-          <div
-            className='p-card__head'
-            style={{ backgroundImage: `url(${avatarUrl})` }}
-          >
-            <div className='p-card__title'>
-              <h2 className='p-card__username'>UNiVeRsE</h2>
-              <span className='p-card__game'>Dota 2 Player</span>
-            </div>
-          </div>
-          <div className='p-card__info-list'>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Name</div>
-              <div className='p-card__info-value'>Saahil Arora</div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>DOB</div>
-              <div className='p-card__info-value'>11.10.1989</div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Country </div>
-              <div className='p-card__info-value'>United States</div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Total Prize Money</div>
-              <div className='p-card__info-value'>
-                $2,720,624
-                <br />
-                (Over 5 years)
-              </div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Tournament wins</div>
-              <div className='p-card__info-value'>20</div>
-            </div>
-          </div>
-          <div className='p-card__desc'>
-            UNiVeRsE obtained $100,000 in total prize money earned on July 21,
-            2014 with a $207,691 cash prize from his 3rd place finish at The
-            International 2014. Prize money from 23 tournaments got him to that
-            point.
-          </div>
-          <div className='p-card__links cf'>
-            <div className='p-card__links__col1'>
-              <div className='p-card__links__team'>
-                <img
-                  className='p-card__links__team-logo'
-                  src={teamLogo}
-                  alt='TeamLogo'
-                />
-                <span className='p-card__links__team-name'>Evil Geniuses</span>
-              </div>
-            </div>
-            <div className='p-card__links__col2'>
-              <a className='p-card__links__twitch' href='# ' target='_blank'>
-                <i className='icon-twitch' aria-hidden='true'></i>
-                /universedota
-              </a>
-              <a className='p-card__links__twitter' href='# ' target='_blank'>
-                <i className='icon-twitter' aria-hidden='true'></i>
-                @universedota
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    ));
-  }
+  const vizData = useContext(VisualizationContext);
+  const cardData = vizData['leaderboards|top-earning-men'].elements;
+
+  const { data } = useContext(TranslationContext);
 
   return (
     <article className='screen screen--sub'>
-      <h1 className='screen__heading'>Top Earning Men</h1>
+      <h1 className='screen__heading'>{data && data.cat2_sub7_title}</h1>
 
       <ul className='screen__desc'></ul>
 
-      <div className='p-card-wrap cf'>{generateCards(10)}</div>
+      <div className='p-card-wrap cf'>
+        {cardData.map((card, index) => (
+          <div key={index} className='p-card'>
+            <div className='p-card__inner'>
+              <div
+                className='p-card__head'
+                style={{ backgroundImage: `url(${card['player-photo']})` }}
+              >
+                <div className='p-card__title'>
+                  <h2 className='p-card__username'>{card['player-id']}</h2>
+                  <span className='p-card__game'>
+                    {card['main-game']} {data && data.cat2_sub7_txt2}
+                  </span>
+                </div>
+              </div>
+              <div className='p-card__info-list'>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub7_txt1}
+                  </div>
+                  <div className='p-card__info-value'>
+                    {card['player-name']}
+                  </div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub7_txt3}
+                  </div>
+                  <div className='p-card__info-value'>
+                    {card['date-of-birth']}
+                  </div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub7_txt4}{' '}
+                  </div>
+                  <div className='p-card__info-value'>{card['country']}</div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub7_txt5}
+                  </div>
+                  <div className='p-card__info-value'>
+                    ${card['total-prize']}
+                    <br />
+                    (Over 5 years)
+                  </div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub7_txt6}
+                  </div>
+                  <div className='p-card__info-value'>
+                    {card['tournament-wins-number']}
+                  </div>
+                </div>
+              </div>
+              <div className='p-card__desc'>{card['description']}</div>
+              <div className='p-card__links cf'>
+                <div className='p-card__links__col1'>
+                  <div className='p-card__links__team'>
+                    <img
+                      className='p-card__links__team-logo'
+                      src={card['team-logo']}
+                      alt='TeamLogo'
+                    />
+                    <span className='p-card__links__team-name'>
+                      {card['team-name']}
+                    </span>
+                  </div>
+                </div>
+                <div className='p-card__links__col2'>
+                  <a
+                    className='p-card__links__twitch'
+                    href={card['twitch-url']}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <i className='icon-twitch' aria-hidden='true'></i>
+                    /universedota
+                  </a>
+                  <a
+                    className='p-card__links__twitter'
+                    href={card['twitter-url']}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <i className='icon-twitter' aria-hidden='true'></i>
+                    @universedota
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </article>
   );
 }
--- src/components/LeaderboardsTopEarningWomen.js
+++ src/components/LeaderboardsTopEarningWomen.js
@@ -1,86 +1,116 @@
-import React from 'react';
-import avatarUrl from '../data/images/1-scarlett.png';
-import teamLogo from '../data/teams/Team-Expert.jpg';
+import React, { useContext } from 'react';
+
+import VisualizationContext from '../contexts/VisualizationContext';
+import TranslationContext from '../contexts/TranslationContext';
 
 function LeaderboardsTopEarningWomen() {
-  function generateCards(count) {
-    return [...Array(count)].map((key, index) => (
-      <div key={index} className='p-card p-card--female'>
-        <div className='p-card__inner'>
-          <div
-            className='p-card__head'
-            style={{ backgroundImage: `url(${avatarUrl})` }}
-          >
-            <div className='p-card__title'>
-              <h2 className='p-card__username'>Scarlett</h2>
-              <span className='p-card__game'>Fighting Game Player</span>
-            </div>
-          </div>
-          <div className='p-card__info-list'>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Name</div>
-              <div className='p-card__info-value'>Sasha Hostyn</div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>DOB</div>
-              <div className='p-card__info-value'>14.12.1993</div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Country </div>
-              <div className='p-card__info-value'>Canada</div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Total Prize Money</div>
-              <div className='p-card__info-value'>
-                $171,425
-                <br />
-                (Over 5 years)
-              </div>
-            </div>
-            <div className='p-card__info-item'>
-              <div className='p-card__info-title'>Tournament wins</div>
-              <div className='p-card__info-value'>20</div>
-            </div>
-          </div>
-          <div className='p-card__desc'>
-            Scarlett obtained $100,000 in total prize money earned on July 27,
-            2014 with a $2,000 cash prize from her 1st place finish at ATC
-            Season 3. Prize money from 45 tournaments got her to that point.
-          </div>
-          <div className='p-card__links cf'>
-            <div className='p-card__links__col1'>
-              <div className='p-card__links__team'>
-                <img
-                  className='p-card__links__team-logo'
-                  src={teamLogo}
-                  alt='TeamLogo'
-                />
-                <span className='p-card__links__team-name'>Team Expert</span>
-              </div>
-            </div>
-            <div className='p-card__links__col2'>
-              <a className='p-card__links__twitch' href='# ' target='_blank'>
-                <i className='icon-twitch' aria-hidden='true'></i>
-                /scarlettm
-              </a>
-              <a className='p-card__links__twitter' href='# ' target='_blank'>
-                <i className='icon-twitter' aria-hidden='true'></i>
-                @onfireScarlett
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    ));
-  }
+  const vizData = useContext(VisualizationContext);
+  const cardData = vizData['leaderboards|top-earning-women'].elements;
+
+  const { data } = useContext(TranslationContext);
 
   return (
     <article className='screen screen--sub'>
-      <h1 className='screen__heading'>Top Earning Women</h1>
+      <h1 className='screen__heading'>{data && data.cat2_sub8_title}</h1>
 
       <ul className='screen__desc'></ul>
 
-      <div className='p-card-wrap cf'>{generateCards(10)}</div>
+      <div className='p-card-wrap cf'>
+        {cardData.map((card, index) => (
+          <div key={index} className='p-card'>
+            <div className='p-card__inner'>
+              <div
+                className='p-card__head'
+                style={{ backgroundImage: `url(${card['player-photo']})` }}
+              >
+                <div className='p-card__title'>
+                  <h2 className='p-card__username'>{card['player-id']}</h2>
+                  <span className='p-card__game'>
+                    {card['main-game']} {data && data.cat2_sub8_txt2}
+                  </span>
+                </div>
+              </div>
+              <div className='p-card__info-list'>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub8_txt1}
+                  </div>
+                  <div className='p-card__info-value'>
+                    {card['player-name']}
+                  </div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub8_txt3}
+                  </div>
+                  <div className='p-card__info-value'>
+                    {card['date-of-birth']}
+                  </div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub8_txt4}{' '}
+                  </div>
+                  <div className='p-card__info-value'>{card['country']}</div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub8_txt5}
+                  </div>
+                  <div className='p-card__info-value'>
+                    ${card['total-prize']}
+                    <br />
+                    (Over 5 years)
+                  </div>
+                </div>
+                <div className='p-card__info-item'>
+                  <div className='p-card__info-title'>
+                    {data && data.cat2_sub8_txt6}
+                  </div>
+                  <div className='p-card__info-value'>
+                    {card['tournament-wins-number']}
+                  </div>
+                </div>
+              </div>
+              <div className='p-card__desc'>{card['description']}</div>
+              <div className='p-card__links cf'>
+                <div className='p-card__links__col1'>
+                  <div className='p-card__links__team'>
+                    <img
+                      className='p-card__links__team-logo'
+                      src={card['team-logo']}
+                      alt='TeamLogo'
+                    />
+                    <span className='p-card__links__team-name'>
+                      {card['team-name']}
+                    </span>
+                  </div>
+                </div>
+                <div className='p-card__links__col2'>
+                  <a
+                    className='p-card__links__twitch'
+                    href={card['twitch-url']}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <i className='icon-twitch' aria-hidden='true'></i>
+                    /universedota
+                  </a>
+                  <a
+                    className='p-card__links__twitter'
+                    href={card['twitter-url']}
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <i className='icon-twitter' aria-hidden='true'></i>
+                    @universedota
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </article>
   );
 }
