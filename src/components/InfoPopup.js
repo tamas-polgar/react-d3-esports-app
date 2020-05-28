e5d875a4f4faf1e2d8e054ff964aa844fbc1d32f
tamas-polgar <tamas.polgar.dev@gmail.com>
2020-05-28 09:24:41 +0200

    Info popup multi language functionality

--- src/components/InfoPopup.js
+++ src/components/InfoPopup.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import TranslationContext from '../contexts/TranslationContext';
 
 function InfoPopup({ open, closeDialog }) {
+  const { lang, data } = useContext(TranslationContext);
+
   return (
     <div
       className='popup-wrap'
@@ -11,42 +15,11 @@ function InfoPopup({ open, closeDialog }) {
         <button className='popup__close-btn' onClick={() => closeDialog()}>
           <i className='icon-close'></i>
         </button>
-        <h3 className='popup__title'>Information</h3>
-        <div className='popup__content'>
-          <p>Champions of eSports is brought to you by Unibet. </p>
-          <p>
-            Viewership data has been sourced from{' '}
-            <a
-              target='_blank'
-              href='https://www.statista.com/statistics/490480/global-esports-audience-size-viewer-type/'
-              rel='noopener noreferrer'
-            >
-              Statista
-            </a>
-            .
-          </p>
-          <p>
-            eSport player, game and country data has been sourced from{' '}
-            <a
-              target='_blank'
-              href='http://www.esportsearnings.com/'
-              rel='noopener noreferrer'
-            >
-              eSportsearnings
-            </a>
-            ,{' '}
-            <a
-              target='_blank'
-              href='http://wiki.teamliquid.net/'
-              rel='noopener noreferrer'
-            >
-              TeamLiquid
-            </a>
-            , Newzoo and Twitch.
-          </p>
-          <p>Population data is supplied by WorldBank.org.</p>
-          <p>Information is up-to-date as of February 2017.</p>
-        </div>
+        <h3 className='popup__title'>{data && data.info_title}</h3>
+        <div
+          className='popup__content'
+          dangerouslySetInnerHTML={{ __html: data && data.info_txt }}
+        />
       </div>
     </div>
   );
