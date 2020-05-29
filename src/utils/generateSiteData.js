36b4e4ee43d94e03d52bb9e13f70bb5c530314c1
tamas-polgar <tamas.polgar.dev@gmail.com>
2020-05-29 07:51:42 +0430

    Updated language data.

--- src/utils/generateSiteData.js
+++ src/utils/generateSiteData.js
@@ -1,11 +1,12 @@
 export const generateSiteData = objArray => {
   let resultEn = {};
   let resultFi = {};
-  objArray['en']['elements'].map(obj => {
-    resultEn[obj.title] = obj.description;
+
+  objArray['language']['elements'].map(obj => {
+    resultEn[obj.title] = obj.en;
   });
-  objArray['fi']['elements'].map(obj => {
-    resultFi[obj.title] = obj.description;
+  objArray['language']['elements'].map(obj => {
+    resultFi[obj.title] = obj.fi;
   });
 
   return { en: resultEn, fi: resultFi };
