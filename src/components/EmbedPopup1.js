1dc43adba93990e90f64669dfa23ec0909f0494d
tamas-polgar <tamas.polgar.dev@gmail.com>
2020-05-29 10:43:03 +0200

    Add embed string to path

--- src/components/EmbedPopup1.js
+++ src/components/EmbedPopup1.js
@@ -1,11 +1,14 @@
 import React, { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import TranslationContext from '../contexts/TranslationContext';
 
 function EmbedPopup1({ open, closeDialog }) {
+const location = useLocation();
   const { lang, data } = useContext(TranslationContext);
-  const copyText = `<div className="e-sports" style="border:1px solid #300;width:100%;height:700px;margin:0 auto;background:#fff;position:relative;"><iframe data-url="https://kindred-esports-react.netlify.app/${lang}" src="https://kindred-esports-react.netlify.app/${lang}" style="position:absolute;top:0;left:0;width:100%;height:100%; border:0;"></iframe></div><div className="meframe"></div><div><a href="https://kindred-esports-react.netlify.app/${lang}" target="_blank" style="cursor: pointer" >The Champions of eSports</a> </div><br/>`;
+  const appUrl = 'https://kindred-esports-react.netlify.app/';
+  const copyText = `<div className="e-sports" style="border:1px solid #300;width:100%;height:700px;margin:0 auto;background:#fff;position:relative;"><iframe data-url="${appUrl}${lang}" src="${appUrl}${lang}" style="position:absolute;top:0;left:0;width:100%;height:100%; border:0;"></iframe></div><div className="meframe"></div><div><a href="${appUrl}${lang}" target="_blank" style="cursor: pointer" >The Champions of eSports</a> </div><br/>`;
 
   return (
     <div
