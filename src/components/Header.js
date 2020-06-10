7fb76d0b590d03b9a8f6306f931f63365431be02
tamas-polgar <tamas.polgar.dev@gmail.com>
2020-06-10 12:48:09 +0300

    Update navbar link to relative path

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,17 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import TranslationContext from '../contexts/TranslationContext';
 
 function Header() {
   const { lang, defaultLanguage, data } = useContext(TranslationContext);
+  const { pathname } = useLocation();
   const prefix = lang === defaultLanguage ? '' : lang;
+  const prepath = pathname.substr(0, pathname.lastIndexOf('/'));
 
   return (
     <header className='header'>
       <div className='header__home' id='header__home'>
-        <Link to={`/${prefix}`}>
+        <Link to={`${prepath}/${prefix}`}>
           <img
             src='assets/img/logo.png'
             alt='Home'
@@ -28,47 +30,61 @@ function Header() {
 
             <ul className='nav__second-level'>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|esports-viewership`}>
+                <Link to={`${prepath}/${prefix}#industry|esports-viewership`}>
                   {data && data.cat1_sub1}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|total-prize-money-awarded`}>
+                <Link
+                  to={`${prepath}/${prefix}#industry|total-prize-money-awarded`}
+                >
                   {data && data.cat1_sub2}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|amount-of-esports-tournaments`}>
+                <Link
+                  to={`${prepath}/${prefix}#industry|amount-of-esports-tournaments`}
+                >
                   {data && data.cat1_sub3}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|number-of-pro-esports-players`}>
+                <Link
+                  to={`${prepath}/${prefix}#industry|number-of-pro-esports-players`}
+                >
                   {data && data.cat1_sub4}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|average-tournament-reward`}>
+                <Link
+                  to={`${prepath}/${prefix}#industry|average-tournament-reward`}
+                >
                   {data && data.cat1_sub5}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|country-winnings-over-time`}>
+                <Link
+                  to={`${prepath}/${prefix}#industry|country-winnings-over-time`}
+                >
                   {data && data.cat1_sub6}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|player-origin-over-time`}>
+                <Link
+                  to={`${prepath}/${prefix}#industry|player-origin-over-time`}
+                >
                   {data && data.cat1_sub7}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|player-density`}>
+                <Link to={`${prepath}/${prefix}#industry|player-density`}>
                   {data && data.cat1_sub8}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#industry|progression-of-countries`}>
+                <Link
+                  to={`${prepath}/${prefix}#industry|progression-of-countries`}
+                >
                   {data && data.cat1_sub9}
                 </Link>
               </li>
@@ -83,54 +99,68 @@ function Header() {
 
             <ul className='nav__second-level'>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-games-by-money`}>
+                <Link
+                  to={`${prepath}/${prefix}#leaderboards|top-games-by-money`}
+                >
                   {data && data.cat2_sub1}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-games-by-players`}>
+                <Link
+                  to={`${prepath}/${prefix}#leaderboards|top-games-by-players`}
+                >
                   {data && data.cat2_sub2}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-games-by-tournaments`}>
+                <Link
+                  to={`${prepath}/${prefix}#leaderboards|top-games-by-tournaments`}
+                >
                   {data && data.cat2_sub3}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-earning-teams`}>
+                <Link
+                  to={`${prepath}/${prefix}#leaderboards|top-earning-teams`}
+                >
                   {data && data.cat2_sub4}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
                 <Link
-                  to={`/${prefix}#leaderboards|top-earning-players-overall`}
+                  to={`${prepath}/${prefix}#leaderboards|top-earning-players-overall`}
                 >
                   {data && data.cat2_sub5}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-female-players`}>
+                <Link
+                  to={`${prepath}/${prefix}#leaderboards|top-female-players`}
+                >
                   {data && data.cat2_sub6}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-earning-men`}>
+                <Link to={`${prepath}/${prefix}#leaderboards|top-earning-men`}>
                   {data && data.cat2_sub7}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-earning-women`}>
+                <Link
+                  to={`${prepath}/${prefix}#leaderboards|top-earning-women`}
+                >
                   {data && data.cat2_sub8}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-earning-countries`}>
+                <Link
+                  to={`${prepath}/${prefix}#leaderboards|top-earning-countries`}
+                >
                   {data && data.cat2_sub9}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#leaderboards|top-winners`}>
+                <Link to={`${prepath}/${prefix}#leaderboards|top-winners`}>
                   {data && data.cat2_sub10}
                 </Link>
               </li>
@@ -145,22 +175,24 @@ function Header() {
 
             <ul className='nav__second-level'>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#players|common-age`}>
+                <Link to={`${prepath}/${prefix}#players|common-age`}>
                   {data && data.cat3_sub1}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#players|average-earnings-by-age`}>
+                <Link
+                  to={`${prepath}/${prefix}#players|average-earnings-by-age`}
+                >
                   {data && data.cat3_sub2}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#players|country-of-origin`}>
+                <Link to={`${prepath}/${prefix}#players|country-of-origin`}>
                   {data && data.cat3_sub3}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#players|male-vs-female`}>
+                <Link to={`${prepath}/${prefix}#players|male-vs-female`}>
                   {data && data.cat3_sub4}
                 </Link>
               </li>
@@ -175,27 +207,31 @@ function Header() {
 
             <ul className='nav__second-level'>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#games|dota-2`}>
+                <Link to={`${prepath}/${prefix}#games|dota-2`}>
                   {data && data.cat4_sub1}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#games|league-of-legends`}>
+                <Link to={`${prepath}/${prefix}#games|league-of-legends`}>
                   {data && data.cat4_sub2}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#games|counter-strike-global-offensive`}>
+                <Link
+                  to={`${prepath}/${prefix}#games|counter-strike-global-offensive`}
+                >
                   {data && data.cat4_sub3}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#games|starcraft-ii`}>
+                <Link to={`${prepath}/${prefix}#games|starcraft-ii`}>
                   {data && data.cat4_sub4}
                 </Link>
               </li>
               <li className='nav__second-level__i'>
-                <Link to={`/${prefix}#games|hearthstone-heroes-of-war`}>
+                <Link
+                  to={`${prepath}/${prefix}#games|hearthstone-heroes-of-war`}
+                >
                   {data && data.cat4_sub5}
                 </Link>
               </li>
